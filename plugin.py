2cd42b4b4f9eae40b91a2a8b77d07964ad215458
anow2008 <92413624+anow2008@users.noreply.github.com>
2026-01-11 19:07:24 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -11,21 +11,18 @@ import os, time
 from Crypto.Cipher import AES
 import base64
 
-# اللغات
 PluginLanguageDomain = "BISSPro"
 PluginLanguagePath = "Extensions/BISSPro/locale"
 bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 def _(txt): return dgettext(PluginLanguageDomain, txt)
 
-# الملفات
 BISS_FILE = "/etc/tuxbox/config/SoftCam.Key"
 USB_PATH = "/media/usb/SoftCam.Key"
 BACKUP_PATH = "/etc/tuxbox/config/SoftCam.Key.bak"
 LOG_FILE = "/usr/lib/enigma2/python/Plugins/Extensions/BISSPro/debug.log"
 SKIN_FILE = "/usr/lib/enigma2/python/Plugins/Extensions/BISSPro/skin.xml"
 
-# تشفير AES
-SECRET_KEY = b"MySecretKey12345"  # 16 bytes
+SECRET_KEY = b"MySecretKey12345"
 DEBUG_MODE = True
 
 def log(msg):
@@ -33,8 +30,7 @@ def log(msg):
         try:
             with open(LOG_FILE, "a") as f:
                 f.write("[%s] %s\n" % (time.strftime("%Y-%m-%d %H:%M:%S"), msg))
-        except:
-            pass
+        except: pass
 
 def encrypt_line(line):
     cipher = AES.new(SECRET_KEY, AES.MODE_ECB)
@@ -48,6 +44,27 @@ def decrypt_line(enc_line):
     decoded = base64.b64decode(enc_line)
     decrypted = cipher.decrypt(decoded).decode().rstrip()
     return decrypted
+
+class AddKeyScreen(Screen):
+    skin = """
+    <screen name="AddKeyScreen" position="center,center" size="600,200" title="Add BISS Key">
+        <widget name="label" position="20,20" size="560,40" font="Regular;22" halign="center" valign="center"/>
+        <widget name="keyinput" position="20,80" size="560,50" font="Regular;24"/>
+    </screen>
+    """
+    def __init__(self, session, sid, callback):
+        Screen.__init__(self, session)
+        self.sid = sid
+        self.callback = callback
+        self["label"] = Label("Channel SID: %s\nEnter BISS / BISS-E Key:" % sid)
+        self["keyinput"] = InputBox()
+        self["actions"] = ActionMap(["OkCancelActions"], {"ok": self.okPressed, "cancel": self.close}, -1)
+
+    def okPressed(self):
+        key = self["keyinput"].getText().replace(" ","").upper()
+        if key:
+            self.callback(key)
+        self.close()
 
 class BISSPro(Screen):
     def __init__(self, session):
@@ -68,12 +85,10 @@ class BISSPro(Screen):
             _("About"),
             _("Exit")
         ]
-
         self["menu"] = MenuList(self.menu)
         self["hint"] = Label("RED:Add  GREEN:View  YELLOW:Delete  BLUE:Restart")
-
         self["actions"] = ActionMap(
-            ["OkCancelActions", "ColorActions"],
+            ["OkCancelActions","ColorActions"],
             {
                 "ok": self.okPressed,
                 "red": self.addKey,
@@ -97,8 +112,7 @@ class BISSPro(Screen):
 
     def getSID(self):
         service = self.session.nav.getCurrentService()
-        if not service:
-            return None, None
+        if not service: return None, None
         info = service.info()
         sid = "%04X" % info.getInfo(info.sSID)
         name = info.getName() or "Unknown"
@@ -109,4 +123,22 @@ class BISSPro(Screen):
         if not sid:
             self.session.open(MessageBox, _("No active channel"), MessageBox.TYPE_ERROR)
             return
-        self.session.openWithCallback(
+        self.session.open(AddKeyScreen, sid, lambda key: self.saveKey(key, sid, name))
+
+    def saveKey(self, key, sid, name):
+        if not key: return
+        if len(key) not in (16,32):
+            self.session.open(MessageBox, _("Invalid Key Length"), MessageBox.TYPE_ERROR)
+            return
+        line = "F %s %s ; %s %s" % (sid, key, name, time.strftime("%Y-%m-%d"))
+        line_enc = encrypt_line(line)
+        if os.path.exists(BISS_FILE):
+            os.system("cp %s %s" % (BISS_FILE, BACKUP_PATH))
+            log("Backup created")
+        with open(BISS_FILE,"a") as f:
+            f.write("\n"+line_enc)
+        self.restartOscam()
+        log("Key added")
+        self.session.open(MessageBox, _("Key saved successfully"), MessageBox.TYPE_INFO)
+
+    # باقي الدوال showKeys, deleteKey, importUSB, restoreBackup, restartOscam, about كما هي...
