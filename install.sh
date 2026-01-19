ee3e1a5a8fc084f6e2f0800ad7ccb9ca6ca8c0fd
anow2008 <92413624+anow2008@users.noreply.github.com>
2026-01-19 15:39:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+TARGET="/usr/lib/enigma2/python/Plugins/Extensions/BissPor"
 cd /usr/lib/enigma2/python/Plugins/Extensions/ || exit 1
 rm -rf BissPor
 git clone https://github.com/anow2008/BissPor.git
 chmod -R 755 BissPor
-killall -9 enigma2
+chmod 755 BissPor/plugin.py
+killall enigma2
