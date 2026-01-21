6dfe746899e9ff5cacffe9fd27b2fbb1ae82bbae
anow2008 <92413624+anow2008@users.noreply.github.com>
2026-01-21 05:35:07 +0200

    Fix target directory name in install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-TARGET="/usr/lib/enigma2/python/Plugins/Extensions/BissPor"
+TARGET="/usr/lib/enigma2/python/Plugins/Extensions/BissPro"
 cd /usr/lib/enigma2/python/Plugins/Extensions/ || exit 1
-rm -rf BissPor
+rm -rf BissPro
 git clone https://github.com/anow2008/BissPor.git
-chmod -R 755 BissPor
-chmod 755 BissPor/plugin.py
+chmod -R 755 BissPro
+chmod 755 BissPro/plugin.py
 killall enigma2
