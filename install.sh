a21ae42654fe169512c1711dfef815c831ab5a1d
anow2008 <92413624+anow2008@users.noreply.github.com>
2026-01-21 05:37:43 +0200

    Fix repository URL for BissPro clone

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 TARGET="/usr/lib/enigma2/python/Plugins/Extensions/BissPro"
 cd /usr/lib/enigma2/python/Plugins/Extensions/ || exit 1
 rm -rf BissPro
-git clone https://github.com/anow2008/BissPor.git
+git clone https://github.com/anow2008/BissPro.git
 chmod -R 755 BissPro
 chmod 755 BissPro/plugin.py
 killall enigma2
